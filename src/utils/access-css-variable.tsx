730719786116545f176fdd6167a46b187e4fdb31
Sean Wong <seanwong24@live.com>
2021-12-12 03:13:58 -0600

    update access-css-variable in utils

--- src/utils/access-css-variable.tsx
+++ src/utils/access-css-variable.tsx
@@ -1,5 +1,9 @@
 export function updateCSSVariable(name: string, value: string, element: HTMLElement) {
-  element?.style.setProperty(name, value);
+  if (value === undefined || value === null) {
+    element.style.setProperty(name, '');
+  } else {
+    element.style.setProperty(name, value);
+  }
 }
 
 export function obtainCSSVariable(name: string, element: HTMLElement, computed = true) {
