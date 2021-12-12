cd73110c85047f5fa4bfd7b8f4c60daf8f4c1b1d
Sean Wong <seanwong24@live.com>
2021-12-12 14:52:49 -0600

    fix wrong package name

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ CORE_PROJECT_PATH=$(pwd)
 REACT_PROJECT_PATH=./packages/react
 
 cd $REACT_PROJECT_PATH
-if npm i --save-exact awesome-layout@$CORE_PROJECT_VERSION ; then
+if npm i --save-exact @awesome-elements/layout@$CORE_PROJECT_VERSION ; then
     npm version $CORE_PROJECT_VERSION
     git add package*.json
     git commit -m "react package updated to $CORE_PROJECT_VERSION"
