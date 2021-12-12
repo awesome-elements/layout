a0b40c34cf79e3a7cf63160d54f5ec2b7dbdb9f3
Sean Wong <seanwong24@live.com>
2021-12-12 02:15:15 -0600

    format document

--- src/utils/observe-resize.tsx
+++ src/utils/observe-resize.tsx
@@ -1,7 +1,7 @@
 export function observeResize(element: HTMLElement, handlers: ((entry: ResizeObserverEntry) => void)[]) {
   const resizeObserver = new ResizeObserver(entries => {
     entries.forEach(entry => {
-      handlers.forEach(handler => handler.call(this, entry))
+      handlers.forEach(handler => handler.call(this, entry));
     });
   });
   resizeObserver.observe(element);
